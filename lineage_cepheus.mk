6f230c1c64b1248a17ffa4cbac63d7ec26cbd416
chematelegram <chfondia@gmail.com>
2025-02-26 16:01:11 +0100

    cepheus: Do not include Accord

--- lineage_cepheus.mk
+++ lineage_cepheus.mk
@@ -24,9 +24,10 @@ $(call inherit-product, device/xiaomi/cepheus/device.mk)
 # Inherit some common EvolutionX stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+BUILD_SEEDVAULT := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_HAS_UDFPS := true
-BUILD_SEEDVAULT := true
+TARGET_INCLUDE_ACCORD = false
 TARGET_USES_MINI_GAPPS := true
 
 # Device identifier
