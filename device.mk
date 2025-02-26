8b225c8c03773e4242ebcff6d979b935020b1581
Michael Bestas <mkbestas@lineageos.org>
2025-02-26 15:43:58 +0100

    cepheus: Drop libvulkan target that is built by default
    Change-Id: Ic5d73508b850fac7c7427caf12eb4a8e87e3deca
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -541,9 +541,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
 # Vulkan
-PRODUCT_PACKAGES += \
-    libvulkan
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
