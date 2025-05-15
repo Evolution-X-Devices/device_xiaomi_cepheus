a1b2ad52a5ea277809a0b945bbf102ccd5d38c67
nhansp <k63.2412960019@ftu.edu.vn>
2025-05-16 03:46:40 +0700

    cepheus: update releasetools.py
    Signed-off-by: nhansp <k63.2412960019@ftu.edu.vn>

--- releasetools.py
+++ releasetools.py
@@ -18,27 +18,21 @@ import common
 import re
 
 def FullOTA_InstallEnd(info):
-  input_zip = info.input_zip
-  OTA_InstallEnd(info, input_zip)
+  OTA_InstallEnd(info)
   return
 
 def IncrementalOTA_InstallEnd(info):
-  input_zip = info.target_zip
-  OTA_InstallEnd(info, input_zip)
+  OTA_InstallEnd(info)
   return
 
-def AddImage(info, dir, input_zip, basename, dest):
-  path = dir + "/" + basename
-  if path not in input_zip.namelist():
-    return
+def AddImage(info, basename, dest):
+  name = basename
+  data = info.input_zip.read("IMAGES/" + basename)
+  common.ZipWriteStr(info.output_zip, name, data)
+  info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
-  data = input_zip.read(path)
-  common.ZipWriteStr(info.output_zip, basename, data)
-  info.script.Print("Flashing {} image".format(dest.split('/')[-1]))
-  info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
-
-def OTA_InstallEnd(info, input_zip):
+def OTA_InstallEnd(info):
   info.script.Print("Patching dtbo and vbmeta images...")
-  AddImage(info, "IMAGES", input_zip, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
-  AddImage(info, "IMAGES", input_zip, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
+  AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
+  AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   return
