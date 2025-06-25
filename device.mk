ecd677427f53de1de899679578ee943f20189a6d
nhansp <nhan.094.vn@gmail.com>
2025-06-25 17:07:57 +0700

    Fix QS lags
    Signed-off-by: nhansp <nhan.094.vn@gmail.com>

--- device.mk
+++ device.mk
@@ -200,7 +200,6 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3.vendor \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
 
