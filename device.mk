af9aee74823a67c4e821197180b73729430fd701
nhansp <nhan.094.vn@gmail.com>
2025-09-29 23:13:01 +0700

    cepheus: hello AIDL LiveDisplay

--- device.mk
+++ device.mk
@@ -275,6 +275,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay-service.sdm
+
+$(call soong_config_set,livedisplay_sdm,enable_dm,false)
+
 # Logging
 SPAMMY_LOG_TAGS := \
     MiStcImpl \
